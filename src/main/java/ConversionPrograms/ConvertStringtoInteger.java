bc68a47412cf1cb1467d839673b45e50aa4e806a
SaiJyothi D <saijyothid12@gmail.com>
2023-03-12 07:16:32 +0530

    new

---
+++ src/main/java/ConversionPrograms/ConvertStringtoInteger.java
@@ -0,0 +1,10 @@
+package ConversionPrograms;
+
+public class ConvertStringtoInteger {
+    public static void main(String[] args) {
+
+        String s = "789";
+        int k = Integer.parseInt(s);
+        System.out.println(s);
+    }
+}
