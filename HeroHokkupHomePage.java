1c6a69226598781f65e3522820a649f0a2502bbc
DSaijyothi <105036750+DSaijyothi@users.noreply.github.com>
2023-03-12 11:11:21 +0530

    Handling dropdowns

--- HeroHokkupHomePage.java
+++ HeroHokkupHomePage.java
@@ -1,37 +1,32 @@
 package VerificationPages;
 
-import com.beust.ah.A;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+
 
 public class HeroHokkupHomePage {
     WebDriver driver;
 
-    public HeroHokkupHomePage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-        this.driver = driver;
+    public HeroHokkupHomePage(WebDriver driver)
+    {
+this.driver=driver;
     }
+    public void some()  {
+        driver.findElement(By.linkText("Dropdown")).click();
+        WebElement drop=driver.findElement(By.id("dropdown"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Select trip=new Select(drop);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        trip.selectByVisibleText("Option 2");
 
-    @FindBy(linkText = "Drag and Drop")
-    WebElement Drag;
-    //Drag and drop
 
 
-    public void setpath() {
-        Drag.click();
     }
-
-public void drop()  {
-    Actions actions=new Actions(driver);
-    WebElement element=driver.findElement(By.id("content"));
-    actions.dragAndDrop(driver.findElement(By.xpath("//div[@class='column' and @id='column-a']//child::header")),driver.findElement(By.xpath("//div[@class='column' and @id='column-b']//child::header"))).clickAndHold().build().perform();
-    driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
-}
-
 }
