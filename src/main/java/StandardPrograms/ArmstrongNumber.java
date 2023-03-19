92da4ad09cb3898e23ff85cc2d46b56863240027
SaiJyothi D <saijyothid12@gmail.com>
2023-03-20 00:00:40 +0530

    ArmStrong Number

---
+++ src/main/java/StandardPrograms/ArmstrongNumber.java
@@ -0,0 +1,50 @@
+package StandardPrograms;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber
+{
+    static boolean isArmstrong(int n)
+    {
+        int temp,digits=0,last=0,sum=0;
+        temp=n;
+        while (temp > 0) {
+            temp=temp/10;
+            digits++;
+
+
+        }
+        temp=n;
+        while (temp > 0)
+        {
+            last=temp%10;
+            sum+=(Math.pow(last,digits));
+            temp=temp/10;
+        }
+        if(n==sum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Limit:");
+        num=sc.nextInt();
+        System.out.println("The Armstrong are:"+num+"is:");
+        for(int i=0;i<=num;i++)
+
+        {
+            if(isArmstrong(i))
+            {
+                System.out.println(i+ " ");
+            }
+        }
+    }
+}
