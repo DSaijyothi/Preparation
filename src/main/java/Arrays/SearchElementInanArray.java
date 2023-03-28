9ece44e7dba2502bfedb0e478ed611512b2aa479
SaiJyothi D <saijyothid12@gmail.com>
2023-03-28 13:55:42 +0530

    Searching strings in an arraylist

---
+++ src/main/java/Arrays/SearchElementInanArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchElementInanArray
+{
+    public static void main(String[] args) {
+        //Array list
+        List<String> listofStrings = new ArrayList<String>();
+        listofStrings.add("CPU");
+        listofStrings.add("HardDisk");
+        listofStrings.add("Assembler");
+        listofStrings.add("socket");
+        listofStrings.add("blower");
+        //Search element
+if(listofStrings.contains("socket"))
+{
+    System.out.println("******************************");
+}
+else
+{
+    System.out.println("$$$$$$$$$$$$$$$");
+}
+
+    }
+}
