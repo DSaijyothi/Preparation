c30458ba37a3aa724bc8d843bc6d33411ee86762
SaiJyothi D <saijyothid12@gmail.com>
2023-04-09 20:41:57 +0530

    Adding elements on linked using iterator

---
+++ src/main/java/Collections/LinkList.java
@@ -0,0 +1,25 @@
+package Collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkList
+{
+    public static void main(String[] args) {
+        //created linked list along with string
+        LinkedList<String>linkedList=new LinkedList<>();
+        //adding list of elements by taking reference of linkedlist
+        linkedList.add("Mango");
+        linkedList.add("Blackcurrent");
+        linkedList.add("OreoMilkShake");
+        System.out.println("Linked list of elements:");
+        //iterating linked list using iteratior
+        Iterator<String> it=linkedList.iterator();
+        while(it.hasNext());
+        {
+        System.out.println(it.next());
+
+
+    }
+}
+}
